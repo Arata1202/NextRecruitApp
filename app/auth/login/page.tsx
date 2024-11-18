9433f01c4690b4501f1cb86a9c18bb20ab60e462
Arata <aratadayoon@yahoo.co.jp>
2024-11-18 20:24:53 +0900

    fix: middleware

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,5 +1,32 @@
+'use client';
+
+import { supabase } from '@/libs/supabase';
+import { User } from '@supabase/supabase-js';
+import { useState, useEffect } from 'react';
+import { useRouter } from 'next/navigation';
 import Login from '@/components/Auth/Login';
 
 export default function LoginPage() {
+  const router = useRouter();
+  const [, setUser] = useState<User | null>(null);
+  const [userChecked, setUserChecked] = useState(false);
+  useEffect(() => {
+    const checkUser = async () => {
+      const {
+        data: { user },
+      } = await supabase.auth.getUser();
+
+      if (user) {
+        router.push('/');
+      } else {
+        setUser(user);
+        setUserChecked(true);
+      }
+    };
+    checkUser();
+  });
+  if (!userChecked) {
+    return null;
+  }
   return <Login />;
 }
--- app/auth/signup/page.tsx
+++ app/auth/signup/page.tsx
@@ -1,5 +1,32 @@
+'use client';
+
+import { supabase } from '@/libs/supabase';
+import { User } from '@supabase/supabase-js';
+import { useState, useEffect } from 'react';
+import { useRouter } from 'next/navigation';
 import SignUp from '@/components/Auth/SignUp';
 
 export default function SignUpPage() {
+  const router = useRouter();
+  const [, setUser] = useState<User | null>(null);
+  const [userChecked, setUserChecked] = useState(false);
+  useEffect(() => {
+    const checkUser = async () => {
+      const {
+        data: { user },
+      } = await supabase.auth.getUser();
+
+      if (user) {
+        router.push('/');
+      } else {
+        setUser(user);
+        setUserChecked(true);
+      }
+    };
+    checkUser();
+  });
+  if (!userChecked) {
+    return null;
+  }
   return <SignUp />;
 }
