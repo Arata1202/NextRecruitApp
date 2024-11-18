900f486f1349f56a73c3a3d3b62df25d9ae1b185
Arata <aratadayoon@yahoo.co.jp>
2024-11-18 18:40:13 +0900

    fix:

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,267 @@
+'use client';
+
+import { useState, useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+import { supabase } from '@/libs/supabase';
+import { User } from '@supabase/supabase-js';
+import { Dialog, DialogBackdrop, DialogPanel, TransitionChild } from '@headlessui/react';
+import {
+  Bars3Icon,
+  CalendarDaysIcon,
+  ChartBarIcon,
+  ChartPieIcon,
+  Cog6ToothIcon,
+  BuildingOffice2Icon,
+  XMarkIcon,
+  ArrowRightStartOnRectangleIcon,
+} from '@heroicons/react/24/outline';
+
+const navigation = [
+  { name: 'ダッシュボード', href: '#', icon: ChartPieIcon, current: true },
+  { name: 'カレンダー', href: '#', icon: CalendarDaysIcon, current: false },
+  { name: '選考中', href: '#', icon: BuildingOffice2Icon, current: false },
+  { name: '自己分析', href: '#', icon: ChartBarIcon, current: false },
+];
+
+function classNames(...classes: (string | undefined | null | boolean)[]): string {
+  return classes.filter(Boolean).join(' ');
+}
+
 export default function Home() {
-  return <div className="text-2xl font-bold">コンテンツ</div>;
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+  const router = useRouter();
+  const [, setUser] = useState<User | null>(null);
+  const [userChecked, setUserChecked] = useState(false);
+
+  // ログイン済みの場合
+  useEffect(() => {
+    const checkUser = async () => {
+      const {
+        data: { user },
+      } = await supabase.auth.getUser();
+
+      if (!user) {
+        router.push('/auth/login');
+      } else {
+        setUser(user);
+        setUserChecked(true);
+      }
+    };
+    checkUser();
+  });
+  if (!userChecked) {
+    return null;
+  }
+
+  const handleLogout = async () => {
+    await supabase.auth.signOut();
+    router.push('/auth/login');
+  };
+
+  return (
+    <div>
+      <Dialog open={sidebarOpen} onClose={setSidebarOpen} className="relative z-50 lg:hidden">
+        <DialogBackdrop
+          transition
+          className="fixed inset-0 bg-gray-900/80 transition-opacity duration-300 ease-linear data-[closed]:opacity-0"
+        />
+
+        <div className="fixed inset-0 flex">
+          <DialogPanel
+            transition
+            className="relative mr-16 flex w-full max-w-xs flex-1 transform transition duration-300 ease-in-out data-[closed]:-translate-x-full"
+          >
+            <TransitionChild>
+              <div className="absolute left-full top-0 flex w-16 justify-center pt-5 duration-300 ease-in-out data-[closed]:opacity-0">
+                <button
+                  type="button"
+                  onClick={() => setSidebarOpen(false)}
+                  className="-m-2.5 p-2.5"
+                >
+                  <span className="sr-only">Close sidebar</span>
+                  <XMarkIcon aria-hidden="true" className="size-6 text-white" />
+                </button>
+              </div>
+            </TransitionChild>
+            {/* Sidebar component, swap this element with another sidebar if you like */}
+            <div className="flex grow flex-col gap-y-5 overflow-y-auto bg-white px-6 pb-4">
+              <div className="flex h-16 shrink-0 items-center">
+                <img
+                  alt="Your Company"
+                  src="https://tailwindui.com/plus/img/logos/mark.svg?color=blue&shade=600"
+                  className="h-8 w-auto"
+                />
+              </div>
+              <nav className="flex flex-1 flex-col">
+                <ul role="list" className="flex flex-1 flex-col gap-y-7">
+                  <li>
+                    <ul role="list" className="-mx-2 space-y-1">
+                      {navigation.map((item) => (
+                        <li key={item.name}>
+                          <a
+                            href={item.href}
+                            className={classNames(
+                              item.current
+                                ? 'bg-gray-50 text-blue-500'
+                                : 'text-gray-700 hover:bg-gray-50 hover:text-blue-500',
+                              'group flex gap-x-3 rounded-md p-2 text-sm/6 font-semibold',
+                            )}
+                          >
+                            <item.icon
+                              aria-hidden="true"
+                              className={classNames(
+                                item.current
+                                  ? 'text-blue-500'
+                                  : 'text-gray-400 group-hover:text-blue-500',
+                                'size-6 shrink-0',
+                              )}
+                            />
+                            {item.name}
+                          </a>
+                        </li>
+                      ))}
+                    </ul>
+                  </li>
+                  <li className="mt-auto">
+                    <ul role="list" className="-mx-2 space-y-1">
+                      <li>
+                        <button
+                          onClick={handleLogout}
+                          className="group -mx-2 flex gap-x-3 rounded-md p-2 text-sm/6 font-semibold text-gray-700 hover:bg-gray-50 hover:text-blue-500"
+                        >
+                          <ArrowRightStartOnRectangleIcon
+                            aria-hidden="true"
+                            className="size-6 shrink-0 text-gray-400 group-hover:text-blue-500"
+                          />
+                          ログアウト
+                        </button>
+                      </li>
+                      <li>
+                        <a
+                          href="#"
+                          className="group -mx-2 flex gap-x-3 rounded-md p-2 text-sm/6 font-semibold text-gray-700 hover:bg-gray-50 hover:text-blue-500"
+                        >
+                          <Cog6ToothIcon
+                            aria-hidden="true"
+                            className="size-6 shrink-0 text-gray-400 group-hover:text-blue-500"
+                          />
+                          設定
+                        </a>
+                      </li>
+                    </ul>
+                  </li>
+                </ul>
+              </nav>
+            </div>
+          </DialogPanel>
+        </div>
+      </Dialog>
+
+      {/* Static sidebar for desktop */}
+      <div className="hidden lg:fixed lg:inset-y-0 lg:z-50 lg:flex lg:w-72 lg:flex-col">
+        {/* Sidebar component, swap this element with another sidebar if you like */}
+        <div className="flex grow flex-col gap-y-5 overflow-y-auto border-r border-gray-200 bg-white px-6 pb-4">
+          <div className="flex h-16 shrink-0 items-center">
+            <img
+              alt="Your Company"
+              src="https://tailwindui.com/plus/img/logos/mark.svg?color=blue&shade=600"
+              className="h-8 w-auto"
+            />
+          </div>
+          <nav className="flex flex-1 flex-col">
+            <ul role="list" className="flex flex-1 flex-col gap-y-7">
+              <li>
+                <ul role="list" className="-mx-2 space-y-1">
+                  {navigation.map((item) => (
+                    <li key={item.name}>
+                      <a
+                        href={item.href}
+                        className={classNames(
+                          item.current
+                            ? 'bg-gray-50 text-blue-500'
+                            : 'text-gray-700 hover:bg-gray-50 hover:text-blue-500',
+                          'group flex gap-x-3 rounded-md p-2 text-sm/6 font-semibold',
+                        )}
+                      >
+                        <item.icon
+                          aria-hidden="true"
+                          className={classNames(
+                            item.current
+                              ? 'text-blue-500'
+                              : 'text-gray-400 group-hover:text-blue-500',
+                            'size-6 shrink-0',
+                          )}
+                        />
+                        {item.name}
+                      </a>
+                    </li>
+                  ))}
+                </ul>
+              </li>
+
+              <li className="mt-auto">
+                <ul role="list" className="-mx-2 space-y-1">
+                  <li>
+                    <button
+                      onClick={handleLogout}
+                      className="group -mx-2 flex gap-x-3 rounded-md p-2 text-sm/6 font-semibold text-gray-700 hover:bg-gray-50 hover:text-blue-500"
+                    >
+                      <ArrowRightStartOnRectangleIcon
+                        aria-hidden="true"
+                        className="size-6 shrink-0 text-gray-400 group-hover:text-blue-500"
+                      />
+                      ログアウト
+                    </button>
+                  </li>
+                  <li>
+                    <a
+                      href="#"
+                      className="group -mx-2 flex gap-x-3 rounded-md p-2 text-sm/6 font-semibold text-gray-700 hover:bg-gray-50 hover:text-blue-500"
+                    >
+                      <Cog6ToothIcon
+                        aria-hidden="true"
+                        className="size-6 shrink-0 text-gray-400 group-hover:text-blue-500"
+                      />
+                      設定
+                    </a>
+                  </li>
+                </ul>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+
+      <div className="lg:pl-72">
+        <div className="SidebarLayout">
+          <div className="sticky top-0 z-40 flex h-16 shrink-0 items-center gap-x-4 border-b border-gray-200 bg-white px-4 shadow-sm sm:gap-x-6 sm:px-6 lg:px-8">
+            <button
+              type="button"
+              onClick={() => setSidebarOpen(true)}
+              className="-m-2.5 p-2.5 text-gray-700 lg:hidden"
+            >
+              <span className="sr-only">Open sidebar</span>
+              <Bars3Icon aria-hidden="true" className="size-6" />
+            </button>
+
+            <div aria-hidden="true" className="h-6 w-px bg-gray-200 lg:hidden" />
+
+            <div className="flex ml-auto">
+              <div className="flex h-16 shrink-0 items-center">
+                <img
+                  alt="Your Company"
+                  src="https://tailwindui.com/plus/img/logos/mark.svg?color=blue&shade=600"
+                  className="h-8 w-auto"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <main className="py-10">
+          <div className="px-4 sm:px-6 lg:px-8">コンテンツ</div>
+        </main>
+      </div>
+    </div>
+  );
 }
