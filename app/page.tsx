f6109b1e50979fe55cae5d0b4a19d6017e0e5095
Arata <aratadayoon@yahoo.co.jp>
2024-11-25 13:41:53 +0900

    #2 fix:

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 'use client';
 
+import Top from '@/components/Home/Pages/Top';
+
 export default function DashBoardPage() {
   return (
     <>
-      <div>top</div>
+      <Top />
     </>
   );
 }
---
+++ components/Home/Pages/Top/index.tsx
@@ -0,0 +1,10 @@
+import Header from '../../Layouts/Header';
+import Footer from '../../Layouts/Footer';
+export default function Top() {
+  return (
+    <>
+      <Header />
+      <Footer />
+    </>
+  );
+}
