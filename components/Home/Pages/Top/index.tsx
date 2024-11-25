a16364d0fa0494a83272d801f055b891fb934072
Arata <aratadayoon@yahoo.co.jp>
2024-11-25 23:26:17 +0900

    fix:

--- components/Home/Pages/Top/index.tsx
+++ components/Home/Pages/Top/index.tsx
@@ -14,7 +14,7 @@ export default function Top() {
               <a
                 href="/service/auth/signup"
                 target="_blank"
-                className="rounded-md bg-blue-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
+                className="rounded-md bg-blue-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-blue-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
               >
                 無料で利用を開始する
               </a>
