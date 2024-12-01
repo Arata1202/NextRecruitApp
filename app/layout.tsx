fe9ec12727b76266f119e0025cea692202a60d6b
Arata <aratadayoon@yahoo.co.jp>
2024-12-01 13:11:14 +0900

    #8 feat: 全体 SEO

--- app/layout.tsx
+++ app/layout.tsx
@@ -52,7 +52,7 @@ export default function RootLayout({
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-            gtag('config', ''G-VGGHGRNHTM', {
+            gtag('config', 'G-VGGHGRNHTM', {
               page_path: window.location.pathname,
             });
           `,
