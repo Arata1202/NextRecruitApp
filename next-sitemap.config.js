673c2799fd39d9681467e98a7a544a2fb7c43590
Arata <aratadayoon@yahoo.co.jp>
2024-12-03 18:19:29 +0900

    #39 fix: layouts sitemap scope

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,5 +2,15 @@ module.exports = {
   siteUrl: 'https://rikuvision.realunivlog.com/',
   generateRobotsTxt: true,
   sitemapSize: 7000,
-  exclude: ['/service', '/service/*'],
+  exclude: ['/service/*', '/service'],
+  additionalPaths: async (config) => [
+    {
+      loc: '/service/auth/signup',
+      lastmod: new Date().toISOString(),
+    },
+    {
+      loc: '/service/auth/login',
+      lastmod: new Date().toISOString(),
+    },
+  ],
 };
