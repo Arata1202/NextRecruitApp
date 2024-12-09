f56ffe532f6bc328f28530cf353a02b864e08981
Arata <aratadayoon@yahoo.co.jp>
2024-12-10 00:00:56 +0900

    #51 feat: カスタムタイトルを除外

--- components/Service/Pages/Analysis/index.tsx
+++ components/Service/Pages/Analysis/index.tsx
@@ -526,8 +526,9 @@ export default function Calendar() {
                             {analysisTitles
                               .filter(
                                 (title) =>
+                                  title.id !== 1 &&
                                   !analyses.some((analysis) => analysis.title === title.title),
-                              ) // 存在するタイトルを除外
+                              )
                               .map((title) => (
                                 <option key={title.id} value={title.id}>
                                   {title.title}
@@ -664,7 +665,7 @@ export default function Calendar() {
                                 const isUsedTitle = analyses.some(
                                   (analysis) => analysis.title === title.title,
                                 );
-                                return isCurrentTitle || !isUsedTitle;
+                                return title.id !== 1 && (isCurrentTitle || !isUsedTitle);
                               })
                               .map((title) => (
                                 <option key={title.id} value={title.id}>
