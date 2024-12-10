69e730c978adad768dcca714eef6f3612b5647fb
Arata <aratadayoon@yahoo.co.jp>
2024-12-10 22:46:23 +0900

    #51 feat: Detail CustomTitle

--- components/Service/Pages/Selection/Detail/index.tsx
+++ components/Service/Pages/Selection/Detail/index.tsx
@@ -21,6 +21,7 @@ type Analysis = {
   id: number;
   title: string;
   description: string;
+  customtitle: string;
 };
 
 type AnalysisTitle = {
@@ -39,7 +40,12 @@ export default function Detail() {
 
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [isEditModalOpen, setIsEditModalOpen] = useState(false);
-  const [editData, setEditData] = useState({ id: 0, titleId: '', description: '' });
+  const [editData, setEditData] = useState({
+    id: 0,
+    titleId: '',
+    description: '',
+    customtitle: '',
+  });
   const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
   const [deleteData, setDeleteData] = useState<Analysis | null>(null);
   const [initialEditTitle, setInitialEditTitle] = useState<string>('');
@@ -48,6 +54,8 @@ export default function Detail() {
   const [descriptionLength, setDescriptionLength] = useState(0);
   const [pageTitle, setPageTitle] = useState<string>('');
   const [isLoading, setIsLoading] = useState(true);
+  const [isCustom, setIsCustom] = useState(false);
+  const [isEditCustom, setEditIsCustom] = useState(false);
 
   const {
     register,
@@ -58,6 +66,7 @@ export default function Detail() {
     defaultValues: {
       titleId: '',
       description: '',
+      customtitle: '',
     },
   });
 
@@ -131,7 +140,8 @@ export default function Detail() {
               title,
               sort
             ),
-            description
+            description,
+            customtitle
           `,
           )
           .eq('selection_id', parseInt(id));
@@ -148,6 +158,7 @@ export default function Detail() {
             id: item.id,
             title: item.title_id?.title || 'Untitled',
             description: item.description,
+            customtitle: item.customtitle,
             sort: item.title_id?.sort || 0,
           }))
           .sort((a, b) => a.sort - b.sort);
@@ -186,7 +197,11 @@ export default function Detail() {
   }, []);
 
   // データ追加
-  const handleAddAnalysis = async (formValues: { titleId: string; description: string }) => {
+  const handleAddAnalysis = async (formValues: {
+    titleId: string;
+    description: string;
+    customtitle: string;
+  }) => {
     if (typeof id !== 'string') {
       console.error('Invalid id:', id);
       return;
@@ -197,6 +212,7 @@ export default function Detail() {
         {
           title_id: parseInt(formValues.titleId),
           description: formValues.description,
+          customtitle: formValues.customtitle,
           selection_id: parseInt(id),
         },
       ])
@@ -208,15 +224,18 @@ export default function Detail() {
     }
 
     if (data) {
-      const addedDetail = {
-        id: data[0]?.id,
-        title:
-          analysisTitles.find((title) => title.id === parseInt(formValues.titleId))?.title ||
-          'Untitled',
-        description: formValues.description,
-      };
+      const addedDetail = data[0];
+      const title = analysisTitles.find((t) => t.id === parseInt(formValues.titleId))?.title || '';
 
-      setAnalyses((prev) => [addedDetail, ...prev]);
+      setAnalyses((prev) => [
+        {
+          id: addedDetail.id,
+          title,
+          description: formValues.description,
+          customtitle: formValues.customtitle,
+        },
+        ...prev,
+      ]);
       setIsModalOpen(false);
       reset();
     }
@@ -229,6 +248,7 @@ export default function Detail() {
         .update({
           title_id: parseInt(editData.titleId),
           description: editData.description,
+          customtitle: editData.customtitle,
         })
         .eq('id', editData.id);
 
@@ -238,16 +258,15 @@ export default function Detail() {
       }
 
       setAnalyses((prev) =>
-        prev.map((detail) =>
-          detail.id === editData.id
+        prev.map((analysis) =>
+          analysis.id === editData.id
             ? {
                 id: editData.id,
-                title:
-                  analysisTitles.find((title) => title.id === parseInt(editData.titleId))?.title ||
-                  'Untitled',
+                title: analysisTitles.find((t) => t.id === parseInt(editData.titleId))?.title || '',
                 description: editData.description,
+                customtitle: editData.customtitle,
               }
-            : detail,
+            : analysis,
         ),
       );
       setIsEditModalOpen(false);
@@ -262,17 +281,24 @@ export default function Detail() {
 
   const openEditModal = (analysis: Analysis) => {
     const titleId = analysisTitles.find((t) => t.title === analysis.title)?.id.toString() || '';
+    const isEditCustom = titleId === '1';
     setEditData({
       id: analysis.id,
       titleId,
       description: analysis.description,
+      customtitle: analysis.customtitle,
     });
     setDescriptionLength(analysis.description.length);
+    setEditIsCustom(isEditCustom);
+
+    const titleForEdit = isEditCustom ? analysis.customtitle : analysis.title;
+    setInitialEditTitle(titleForEdit);
+
     reset({
-      titleId,
+      titleId: isEditCustom ? '1' : titleId,
       description: analysis.description,
+      customtitle: isEditCustom ? analysis.customtitle : '',
     });
-    setInitialEditTitle(analysis.title);
     setIsEditModalOpen(true);
   };
 
@@ -295,15 +321,24 @@ export default function Detail() {
   };
 
   const openDeleteModal = (analysis: Analysis) => {
-    setDeleteData(analysis);
+    const isCustom = analysisTitles.find((t) => t.title === analysis.title)?.id === 1;
+    const titleForDelete = isCustom ? analysis.customtitle : analysis.title;
+
+    setDeleteData({
+      ...analysis,
+      title: titleForDelete,
+    });
     setIsDeleteModalOpen(true);
   };
 
   // 検索処理
   useEffect(() => {
-    const results = analyses.filter((analysis) =>
-      analysis.title.toLowerCase().includes(searchTerm.toLowerCase()),
-    );
+    const results = analyses.filter((analysis) => {
+      const searchLower = searchTerm.toLowerCase();
+      const titleMatch = analysis.title.toLowerCase().includes(searchLower);
+      const customTitleMatch = analysis.customtitle?.toLowerCase().includes(searchLower);
+      return titleMatch || customTitleMatch;
+    });
     setFilteredAnalyses(results);
   }, [searchTerm, analyses]);
 
@@ -477,7 +512,11 @@ export default function Detail() {
                   >
                     <div>
                       <div className="px-4 py-3 sm:px-6 flex">
-                        <h3 className="text-base/7 font-semibold">{analysis.title}</h3>
+                        <h3 className="text-base/7 font-semibold">
+                          {analysis.title === 'カスタムタイトル'
+                            ? analysis.customtitle || 'カスタムタイトルが設定されていません。'
+                            : analysis.title}
+                        </h3>
                         <div className="flex ml-auto">
                           <button
                             type="button"
@@ -544,14 +583,24 @@ export default function Detail() {
                         <select
                           {...register('titleId', { required: 'タイトルを選択してください' })}
                           style={{ height: '36px' }}
+                          onChange={(e) => {
+                            const value = e.target.value;
+                            if (value === '1') {
+                              setIsCustom(true);
+                            } else {
+                              setIsCustom(false);
+                            }
+                          }}
                           className="Search mt-2 block w-full rounded-md border-0 py-1.5 pl-3 pr-10 ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-blue-500 sm:text-sm/6"
                         >
                           <option value="">タイトルを選択</option>
                           {analysisTitles
-                            .filter(
-                              (title) =>
-                                !analyses.some((analysis) => analysis.title === title.title),
-                            )
+                            .filter((title) => {
+                              if (title.id === 1) {
+                                return true;
+                              }
+                              return !analyses.some((analysis) => analysis.title === title.title);
+                            })
                             .map((title) => (
                               <option key={title.id} value={title.id}>
                                 {title.title}
@@ -561,6 +610,41 @@ export default function Detail() {
                         {errors.titleId && (
                           <p className="text-red-500 mt-1 text-left">{errors.titleId.message}</p>
                         )}
+                      </div>
+
+                      {isCustom && (
+                        <div className="mb-4">
+                          <input
+                            {...register('customtitle', { required: 'タイトルを入力してください' })}
+                            placeholder="カスタムタイトル"
+                            className="w-full rounded-md border border-gray-300 p-2 placeholder:text-gray-500"
+                          />
+                          {errors.customtitle && (
+                            <p className="text-red-500 mt-1 text-left">
+                              {errors.customtitle.message}
+                            </p>
+                          )}
+                        </div>
+                      )}
+
+                      <div className="mb-4 flex">
+                        <label>
+                          <input
+                            type="checkbox"
+                            checked={isCustom}
+                            onChange={(e) => {
+                              const isChecked = e.target.checked;
+                              setIsCustom(isChecked);
+                              if (isChecked) {
+                                reset({ titleId: '1' });
+                              } else {
+                                reset({ titleId: '' });
+                              }
+                            }}
+                            className="mr-2"
+                          />
+                          カスタムタイトル
+                        </label>
                       </div>
 
                       <div className="mb-4">
@@ -590,6 +674,7 @@ export default function Detail() {
                           titleId: '',
                           description: '',
                         });
+                        setIsCustom(false);
                       }}
                       className={`DialogButton mt-3 inline-flex w-full justify-center rounded-md px-3 py-2 text-sm font-semibold shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto`}
                     >
@@ -641,9 +726,15 @@ export default function Detail() {
                         <select
                           {...register('titleId', { required: 'タイトルを選択してください' })}
                           style={{ height: '36px' }}
+                          value={editData.titleId}
                           onChange={(e) => {
                             const value = e.target.value;
                             setEditData({ ...editData, titleId: value });
+                            if (value === '1') {
+                              setEditIsCustom(true);
+                            } else {
+                              setEditIsCustom(false);
+                            }
                           }}
                           className="Search mt-2 block w-full rounded-md border-0 py-1.5 pl-3 pr-10 ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-blue-500 sm:text-sm/6"
                         >
@@ -665,6 +756,45 @@ export default function Detail() {
                         {errors.titleId && (
                           <p className="text-red-500 mt-1 text-left">{errors.titleId.message}</p>
                         )}
+                      </div>
+
+                      {isEditCustom && (
+                        <div className="mb-4">
+                          <input
+                            {...register('customtitle', { required: 'タイトルを入力してください' })}
+                            onChange={(e) => {
+                              const value = e.target.value;
+                              setEditData({ ...editData, customtitle: value });
+                            }}
+                            placeholder="カスタムタイトル"
+                            className="w-full rounded-md border border-gray-300 p-2 placeholder:text-gray-500"
+                          />
+                          {errors.customtitle && (
+                            <p className="text-red-500 mt-1 text-left">
+                              {errors.customtitle.message}
+                            </p>
+                          )}
+                        </div>
+                      )}
+
+                      <div className="mb-4 flex">
+                        <label>
+                          <input
+                            type="checkbox"
+                            checked={isEditCustom}
+                            onChange={(e) => {
+                              const isEditChecked = e.target.checked;
+                              setEditIsCustom(isEditChecked);
+                              if (isEditChecked) {
+                                setEditData((prev) => ({ ...prev, titleId: '1' }));
+                              } else {
+                                setEditData((prev) => ({ ...prev, titleId: '' }));
+                              }
+                            }}
+                            className="mr-2"
+                          />
+                          カスタムタイトル
+                        </label>
                       </div>
 
                       <div className="mb-4">
@@ -699,6 +829,7 @@ export default function Detail() {
                           titleId: '',
                           description: '',
                         });
+                        setEditIsCustom(false);
                       }}
                       className={`DialogButton mt-3 inline-flex w-full justify-center rounded-md px-3 py-2 text-sm font-semibold shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto`}
                     >
