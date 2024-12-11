65bea8887e08a90ad5a026817a115389d824f41c
Arata <aratadayoon@yahoo.co.jp>
2024-12-11 16:26:03 +0900

    #76 fix: Alert

--- components/Service/Common/Alert/ConfirmAlert/index.tsx
+++ components/Service/Common/Alert/ConfirmAlert/index.tsx
@@ -13,7 +13,7 @@ const ConfirmAlert: React.FC<AlertProps> = ({ show, onClose, title, message }) =
   return (
     <div
       aria-live="assertive"
-      className="confirmAlert pointer-events-none fixed inset-0 flex items-end px-4 py-6 sm:items-start sm:p-6"
+      className="confirmAlert pointer-events-none fixed inset-0 flex items-start px-4 py-6 sm:items-start sm:p-6"
     >
       <div className="flex w-full flex-col items-center space-y-4 sm:items-end">
         <Transition
