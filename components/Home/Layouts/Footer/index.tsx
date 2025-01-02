2a2a068e330f9e35bf56919aa2c85be80a0499c1
Arata <aratadayoon@yahoo.co.jp>
2025-01-02 14:42:29 +0900

    fix: FontSize

--- components/Home/Layouts/Footer/index.tsx
+++ components/Home/Layouts/Footer/index.tsx
@@ -131,7 +131,7 @@ export default function Footer() {
           </div>
         </div>
         <div className="mt-16 border-t border-gray-300 pt-8 sm:mt-20 lg:mt-24">
-          <p className="text-sm/6 ">Copyright © 2024 リクビジョン All Rights Reserved.</p>
+          <p className="text-xs ">Copyright © 2024 リクビジョン All Rights Reserved.</p>
         </div>
       </div>
     </footer>
