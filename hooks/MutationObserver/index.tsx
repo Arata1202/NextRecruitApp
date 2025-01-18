b19dddb163267640d7e98f7148105d4320126b73
Arata <aratadayoon@yahoo.co.jp>
2025-01-19 00:41:19 +0900

    fix: HeightGuardObserver

--- hooks/MutationObserver/index.tsx
+++ hooks/MutationObserver/index.tsx
@@ -2,25 +2,27 @@ import { useEffect } from 'react';
 
 export const useHeightGuardObserver = () => {
   useEffect(() => {
-    const targets = document.querySelectorAll<HTMLElement>('.mut-height-guard');
-    const observers: MutationObserver[] = [];
+    setTimeout(() => {
+      const targets = document.querySelectorAll<HTMLElement>('.mut-height-guard');
+      const observers: MutationObserver[] = [];
 
-    targets.forEach((target) => {
-      const heightChangeObserver = new MutationObserver(() => {
-        target.style.height = '';
-        target.style.minHeight = '';
+      targets.forEach((target) => {
+        const heightChangeObserver = new MutationObserver(() => {
+          target.style.height = '';
+          target.style.minHeight = '';
+        });
+
+        heightChangeObserver.observe(target, {
+          attributes: true,
+          attributeFilter: ['style'],
+        });
+
+        observers.push(heightChangeObserver);
       });
 
-      heightChangeObserver.observe(target, {
-        attributes: true,
-        attributeFilter: ['style'],
-      });
-
-      observers.push(heightChangeObserver);
-    });
-
-    return () => {
-      observers.forEach((observer) => observer.disconnect());
-    };
+      return () => {
+        observers.forEach((observer) => observer.disconnect());
+      };
+    }, 100);
   }, []);
 };
