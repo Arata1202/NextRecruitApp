8e9507ca69c66fc9a2117ff377ffe39c07d7a5e8
Arata <aratadayoon@yahoo.co.jp>
2025-02-25 17:52:57 +0900

    fix: CloudFlare

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import { withSentryConfig } from '@sentry/nextjs';
 import type { NextConfig } from 'next';
+import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
 
 const nextConfig: NextConfig = {};
+
+if (process.env.NODE_ENV === 'development') {
+  (async () => {
+    await setupDevPlatform();
+  })();
+}
 
 export default withSentryConfig(nextConfig, {
   // For all available options, see:
