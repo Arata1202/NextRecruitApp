18bb41d0e55e15bfbc4f9fa7d040eebdfec8a089
Arata <aratadayoon@yahoo.co.jp>
2025-03-12 15:45:29 +0900

    change: styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import type { Metadata } from 'next';
 import localFont from 'next/font/local';
-import './globals.css';
+import '../styles/globals.css';
 import type { Viewport } from 'next';
 import ScrollTopButton from '@/components/Common/Layouts/ScrollToTop';
 import GoogleAdSense from '@/components/Common/ThirdParties/GoogleAdSense';
