9f49759481fb875a8e0f92ac32e91595754021d5
Arata <aratadayoon@yahoo.co.jp>
2025-03-13 23:04:09 +0900

    refactor: ChildrenModal

--- src/components/Common/Layouts/MainLayout/index.tsx
+++ src/components/Common/Layouts/MainLayout/index.tsx
@@ -2,7 +2,7 @@
 
 import Link from 'next/link';
 import { supabase } from '@/libs/supabase';
-import { useState, useRef, Fragment } from 'react';
+import { useState, useRef } from 'react';
 import { useRouter, usePathname } from 'next/navigation';
 import { isAndroid, isIOS } from 'react-device-detect';
 import styles from './index.module.css';
@@ -16,13 +16,7 @@ import {
   DevicePhoneMobileIcon,
   ArrowUpOnSquareIcon,
 } from '@heroicons/react/24/outline';
-import {
-  Dialog,
-  DialogBackdrop,
-  DialogPanel,
-  TransitionChild,
-  Transition,
-} from '@headlessui/react';
+import { Dialog, DialogBackdrop, DialogPanel, TransitionChild } from '@headlessui/react';
 import {
   TwitterShareButton,
   TwitterIcon,
@@ -40,6 +34,7 @@ import {
   LinkedinIcon,
 } from 'react-share';
 import BooleanModal from '../../Modal/BooleanModal';
+import ChildrenModal from '../../Modal/ChildrenModal';
 
 export default function MainLayout() {
   const pathname = usePathname();
@@ -309,188 +304,102 @@ export default function MainLayout() {
         confirmText="ログアウト"
       />
 
-      <Transition.Root show={shareOpen} as={Fragment}>
-        <Dialog
-          as="div"
-          className="relative z-50"
-          initialFocus={cancelButtonRef}
-          onClose={() => {}}
-        >
-          <Transition.Child
-            as={Fragment}
-            enter="ease-out duration-300"
-            enterFrom="opacity-0"
-            enterTo="opacity-100"
-            leave="ease-in duration-200"
-            leaveFrom="opacity-100"
-            leaveTo="opacity-0"
+      <ChildrenModal
+        open={shareOpen}
+        title="シェアする"
+        Icon={ShareIcon}
+        cancelButtonRef={cancelButtonRef}
+        onClose={handleShareCancel}
+        cancelText="閉じる"
+      >
+        <div className="flex justify-center mt-4">
+          <TwitterShareButton
+            aria-label="シェアボタン"
+            url="https://rikuvision.realunivlog.com"
+            title="リクビジョン"
+            className="m-1 hover:opacity-60"
           >
-            <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
-          </Transition.Child>
+            <TwitterIcon size={40} round={true} />
+          </TwitterShareButton>
 
-          <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
-            <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
-              <Transition.Child
-                as={Fragment}
-                enter="ease-out duration-300"
-                enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-                enterTo="opacity-100 translate-y-0 sm:scale-100"
-                leave="ease-in duration-200"
-                leaveFrom="opacity-100 translate-y-0 sm:scale-100"
-                leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-              >
-                <Dialog.Panel
-                  style={{ width: '100%' }}
-                  className={`m-auto text-gray-700 bg-white relative transform overflow-hidden rounded-lg px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg sm:p-6`}
-                >
-                  <div className="sm:flex sm:items-start">
-                    <div
-                      className="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full sm:mx-0 sm:h-10 sm:w-10"
-                      style={{ backgroundColor: '#eaf4fc' }}
-                    >
-                      <ShareIcon className="h-6 w-6 text-blue-500" aria-hidden="true" />
-                    </div>
-                    <div className="mt-2 text-center sm:ml-4 sm:text-left">
-                      <Dialog.Title as="h1" className={`text-base font-bold leading-6`}>
-                        シェアする
-                      </Dialog.Title>
-                    </div>
-                  </div>
-                  <div className="flex justify-center mt-4">
-                    <TwitterShareButton
-                      aria-label="シェアボタン"
-                      url="https://rikuvision.realunivlog.com"
-                      title="リクビジョン"
-                      className="m-1 hover:opacity-60"
-                    >
-                      <TwitterIcon size={40} round={true} />
-                    </TwitterShareButton>
+          <FacebookShareButton
+            aria-label="シェアボタン"
+            url="https://rikuvision.realunivlog.com"
+            title="リクビジョン"
+            className="m-1 hover:opacity-60"
+          >
+            <FacebookIcon size={40} round={true} />
+          </FacebookShareButton>
 
-                    <FacebookShareButton
-                      aria-label="シェアボタン"
-                      url="https://rikuvision.realunivlog.com"
-                      title="リクビジョン"
-                      className="m-1 hover:opacity-60"
-                    >
-                      <FacebookIcon size={40} round={true} />
-                    </FacebookShareButton>
+          <LineShareButton
+            aria-label="シェアボタン"
+            url="https://rikuvision.realunivlog.com"
+            title="リクビジョン"
+            className="m-1 hover:opacity-60"
+          >
+            <LineIcon size={40} round={true} />
+          </LineShareButton>
 
-                    <LineShareButton
-                      aria-label="シェアボタン"
-                      url="https://rikuvision.realunivlog.com"
-                      title="リクビジョン"
-                      className="m-1 hover:opacity-60"
-                    >
-                      <LineIcon size={40} round={true} />
-                    </LineShareButton>
+          <HatenaShareButton
+            aria-label="シェアボタン"
+            url="https://rikuvision.realunivlog.com"
+            title="リクビジョン"
+            className="m-1 hover:opacity-60"
+          >
+            <HatenaIcon size={40} round={true} />
+          </HatenaShareButton>
 
-                    <HatenaShareButton
-                      aria-label="シェアボタン"
-                      url="https://rikuvision.realunivlog.com"
-                      title="リクビジョン"
-                      className="m-1 hover:opacity-60"
-                    >
-                      <HatenaIcon size={40} round={true} />
-                    </HatenaShareButton>
+          <PinterestShareButton
+            aria-label="シェアボタン"
+            url="https://rikuvision.realunivlog.com"
+            media="/images/og/1.png"
+            description="リクビジョン"
+            className="m-1 hover:opacity-60"
+          >
+            <PinterestIcon size={40} round={true} />
+          </PinterestShareButton>
 
-                    <PinterestShareButton
-                      aria-label="シェアボタン"
-                      url="https://rikuvision.realunivlog.com"
-                      media="/images/og/1.png"
-                      description="リクビジョン"
-                      className="m-1 hover:opacity-60"
-                    >
-                      <PinterestIcon size={40} round={true} />
-                    </PinterestShareButton>
+          <RedditShareButton
+            aria-label="シェアボタン"
+            url="https://rikuvision.realunivlog.com"
+            title="リクビジョン"
+            className="m-1 hover:opacity-60"
+          >
+            <RedditIcon size={40} round={true} />
+          </RedditShareButton>
 
-                    <RedditShareButton
-                      aria-label="シェアボタン"
-                      url="https://rikuvision.realunivlog.com"
-                      title="リクビジョン"
-                      className="m-1 hover:opacity-60"
-                    >
-                      <RedditIcon size={40} round={true} />
-                    </RedditShareButton>
+          <LinkedinShareButton
+            aria-label="シェアボタン"
+            url="https://rikuvision.realunivlog.com"
+            title="リクビジョン"
+            summary="就活における日程管理や自己分析、選考状況などを一括で管理することのできるサービスです。"
+            className="m-1 hover:opacity-60"
+          >
+            <LinkedinIcon size={40} round={true} />
+          </LinkedinShareButton>
+        </div>
+      </ChildrenModal>
 
-                    <LinkedinShareButton
-                      aria-label="シェアボタン"
-                      url="https://rikuvision.realunivlog.com"
-                      title="リクビジョン"
-                      summary="就活における日程管理や自己分析、選考状況などを一括で管理することのできるサービスです。"
-                      className="m-1 hover:opacity-60"
-                    >
-                      <LinkedinIcon size={40} round={true} />
-                    </LinkedinShareButton>
-                  </div>
-                  <div className="mt-5 sm:mt-6 grid grid-flow-row-dense gap-3">
-                    <button
-                      type="button"
-                      className={`DialogButton mt-3 inline-flex w-full justify-center rounded-md px-3 py-2 text-sm font-semibold shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto`}
-                      onClick={handleShareCancel}
-                      ref={cancelButtonRef}
-                    >
-                      キャンセル
-                    </button>
-                  </div>
-                </Dialog.Panel>
-              </Transition.Child>
+      <ChildrenModal
+        open={A2hsOpen}
+        title="アプリを追加する"
+        Icon={DevicePhoneMobileIcon}
+        cancelButtonRef={cancelButtonRef}
+        onClose={handleClose}
+        cancelText="閉じる"
+      >
+        <div className="text-sm text-gray-500 text-left mt-4">
+          <div className="text-left flex">
+            ブラウザの
+            <div className="flex">
+              「シェアアイコン
+              <ArrowUpOnSquareIcon aria-hidden="true" className="h-4 w-4" />
+              」をタップして
             </div>
           </div>
-        </Dialog>
-      </Transition.Root>
-
-      <Transition.Root show={A2hsOpen} as={Fragment}>
-        <Dialog as="div" className="text-gray-700 relative z-50" onClose={() => {}}>
-          <DialogBackdrop
-            transition
-            className="fixed inset-0 bg-gray-500/75 transition-opacity data-[closed]:opacity-0 data-[enter]:duration-300 data-[leave]:duration-200 data-[enter]:ease-out data-[leave]:ease-in"
-          />
-
-          <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
-            <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
-              <DialogPanel
-                transition
-                style={{ width: '100%' }}
-                className="m-auto relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all data-[closed]:translate-y-4 data-[closed]:opacity-0 data-[enter]:duration-300 data-[leave]:duration-200 data-[enter]:ease-out data-[leave]:ease-in sm:my-8 sm:w-full sm:max-w-lg sm:p-6 data-[closed]:sm:translate-y-0 data-[closed]:sm:scale-95"
-              >
-                <div className="mt-3 text-center sm:mt-0 sm:text-left">
-                  <div className="sm:flex sm:items-start">
-                    <div className="mx-auto flex size-12 shrink-0 items-center justify-center rounded-full bg-blue-100 sm:mx-0 sm:size-10">
-                      <DevicePhoneMobileIcon aria-hidden="true" className="size-6 text-blue-500" />
-                    </div>
-                    <div className="mt-2 text-center sm:ml-4 sm:text-left">
-                      <Dialog.Title as="h1" className={`text-base font-bold leading-6`}>
-                        アプリを追加する
-                      </Dialog.Title>
-                      <div className="mt-2">
-                        <div className="text-sm text-gray-500 text-left">
-                          <div className="text-left flex">
-                            ブラウザの
-                            <div className="flex">
-                              「シェアアイコン
-                              <ArrowUpOnSquareIcon aria-hidden="true" className="h-4 w-4" />
-                              」をタップして
-                            </div>
-                          </div>
-                          「ホーム画面に追加」を選択してください。
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                <div className="mt-3">
-                  <button
-                    onClick={handleClose}
-                    className={`DialogButton mt-3 inline-flex w-full justify-center rounded-md px-3 py-2 text-sm font-semibold shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto`}
-                  >
-                    閉じる
-                  </button>
-                </div>
-              </DialogPanel>
-            </div>
-          </div>
-        </Dialog>
-      </Transition.Root>
+          「ホーム画面に追加」を選択してください。
+        </div>
+      </ChildrenModal>
     </>
   );
 }
