578cc36046bc2ee05698367babaa252d1be5f864
Arata <aratadayoon@yahoo.co.jp>
2025-03-13 18:29:18 +0900

    refactor: footer

--- src/components/Common/Layouts/Footer/index.tsx
+++ src/components/Common/Layouts/Footer/index.tsx
@@ -2,83 +2,40 @@ import Link from 'next/link';
 import styles from './index.module.css';
 
 const navigation = {
-  solutions: [
+  about: [
     { name: 'アカウント登録', href: '/service/auth/signup' },
     { name: 'ログイン', href: '/service/auth/login' },
   ],
-  support: [{ name: 'リアル大学生', href: 'https://realunivlog.com' }],
-  company: [
+  developer: [{ name: 'リアル大学生', href: 'https://realunivlog.com' }],
+  policy: [
     { name: 'プライバシーポリシー', href: '/privacy' },
     { name: '免責事項', href: '/disclaimer' },
     { name: '著作権', href: '/copyright' },
     { name: 'リンク', href: '/link' },
   ],
-  legal: [{ name: 'お問い合わせ', href: '/contact' }],
-  social: [
-    {
-      name: 'X',
-      href: 'https://x.com/rikuvision',
-      icon: (props: React.SVGProps<SVGSVGElement>) => (
-        <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
-          <path d="M13.6823 10.6218L20.2391 3H18.6854L12.9921 9.61788L8.44486 3H3.2002L10.0765 13.0074L3.2002 21H4.75404L10.7663 14.0113L15.5685 21H20.8131L13.6819 10.6218H13.6823ZM11.5541 13.0956L10.8574 12.0991L5.31391 4.16971H7.70053L12.1742 10.5689L12.8709 11.5655L18.6861 19.8835H16.2995L11.5541 13.096V13.0956Z" />
-        </svg>
-      ),
-    },
-    {
-      name: 'Instagram',
-      href: 'https://www.instagram.com/riku.vision',
-      icon: (props: React.SVGProps<SVGSVGElement>) => (
-        <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
-          <path
-            fillRule="evenodd"
-            d="M12.315 2c2.43 0 2.784.013 3.808.06 1.064.049 1.791.218 2.427.465a4.902 4.902 0 011.772 1.153 4.902 4.902 0 011.153 1.772c.247.636.416 1.363.465 2.427.048 1.067.06 1.407.06 4.123v.08c0 2.643-.012 2.987-.06 4.043-.049 1.064-.218 1.791-.465 2.427a4.902 4.902 0 01-1.153 1.772 4.902 4.902 0 01-1.772 1.153c-.636.247-1.363.416-2.427.465-1.067.048-1.407.06-4.123.06h-.08c-2.643 0-2.987-.012-4.043-.06-1.064-.049-1.791-.218-2.427-.465a4.902 4.902 0 01-1.772-1.153 4.902 4.902 0 01-1.153-1.772c-.247-.636-.416-1.363-.465-2.427-.047-1.024-.06-1.379-.06-3.808v-.63c0-2.43.013-2.784.06-3.808.049-1.064.218-1.791.465-2.427a4.902 4.902 0 011.153-1.772A4.902 4.902 0 015.45 2.525c.636-.247 1.363-.416 2.427-.465C8.901 2.013 9.256 2 11.685 2h.63zm-.081 1.802h-.468c-2.456 0-2.784.011-3.807.058-.975.045-1.504.207-1.857.344-.467.182-.8.398-1.15.748-.35.35-.566.683-.748 1.15-.137.353-.3.882-.344 1.857-.047 1.023-.058 1.351-.058 3.807v.468c0 2.456.011 2.784.058 3.807.045.975.207 1.504.344 1.857.182.466.399.8.748 1.15.35.35.683.566 1.15.748.353.137.882.3 1.857.344 1.054.048 1.37.058 4.041.058h.08c2.597 0 2.917-.01 3.96-.058.976-.045 1.505-.207 1.858-.344.466-.182.8-.398 1.15-.748.35-.35.566-.683.748-1.15.137-.353.3-.882.344-1.857.048-1.055.058-1.37.058-4.041v-.08c0-2.597-.01-2.917-.058-3.96-.045-.976-.207-1.505-.344-1.858a3.097 3.097 0 00-.748-1.15 3.098 3.098 0 00-1.15-.748c-.353-.137-.882-.3-1.857-.344-1.023-.047-1.351-.058-3.807-.058zM12 6.865a5.135 5.135 0 110 10.27 5.135 5.135 0 010-10.27zm0 1.802a3.333 3.333 0 100 6.666 3.333 3.333 0 000-6.666zm5.338-3.205a1.2 1.2 0 110 2.4 1.2 1.2 0 010-2.4z"
-            clipRule="evenodd"
-          />
-        </svg>
-      ),
-    },
-  ],
+  contact: [{ name: 'お問い合わせ', href: '/contact' }],
 };
 
 export default function Footer() {
   return (
-    <footer
-      className={`${styles.footer} w-full bg-white`}
-      style={{
-        paddingBottom: `calc(40px + env(safe-area-inset-bottom))`,
-      }}
-    >
+    <footer className={`${styles.footer} w-full bg-white`}>
       <div className="mx-auto max-w-7xl px-6 pb-8 pt-16 sm:pt-24 lg:px-8 lg:pt-32">
         <div className="xl:grid xl:grid-cols-3 xl:gap-8">
           <div className="space-y-8">
-            <Link href="/" className="underLogo">
+            <Link href="/" className={styles.logo}>
               <img
-                alt="Company name"
+                alt="タイトル"
                 src="/images/head/1.png"
-                className="hover:scale-110 transition-transform"
-                style={{ height: '45px' }}
+                className={`${styles.image} hover:scale-110 transition-transform`}
               />
             </Link>
-            {/* <div className="flex gap-x-6 underLogo">
-              {navigation.social.map((item) => (
-                <Link
-                  key={item.name}
-                  href={item.href}
-                  target="_blank"
-                  className=" hover:text-blue-500"
-                >
-                  <span className="sr-only">{item.name}</span>
-                  <item.icon aria-hidden="true" className="size-6" />
-                </Link>
-              ))}
-            </div> */}
           </div>
           <div className="mt-16 grid grid-cols-2 gap-8 xl:col-span-2 xl:mt-0">
             <div className="md:grid md:grid-cols-2 md:gap-8">
-              <div className="footerMenu">
+              <div className={styles.menuItem}>
                 <h3 className="text-sm/6 font-semibold ">リクビジョン</h3>
-                <ul role="list" className="mt-6 space-y-4">
-                  {navigation.solutions.map((item) => (
+                <ul className="mt-6 space-y-4">
+                  {navigation.about.map((item) => (
                     <li key={item.name}>
                       <Link href={item.href} className="text-sm/6  hover:text-blue-500">
                         {item.name}
@@ -87,28 +44,15 @@ export default function Footer() {
                   ))}
                 </ul>
               </div>
-              <div className="mt-10 md:mt-0 footerMenu">
+              <div className={`${styles.menuItem} mt-10 md:mt-0`}>
                 <h3 className="text-sm/6 font-semibold ">デベロッパー</h3>
-                {/* <ul role="list" className="mt-6 space-y-4">
-                  {navigation.support.map((item) => (
-                    <li key={item.name}>
-                      <Link
-                        href={item.href}
-                        target="_blank"
-                        className="text-sm/6  hover:text-blue-500"
-                      >
-                        {item.name}
-                      </Link>
-                    </li>
-                  ))}
-                </ul> */}
               </div>
             </div>
-            <div className="md:grid md:grid-cols-2 md:gap-8 footerMenu">
+            <div className={`${styles.menuItem} md:grid md:grid-cols-2 md:gap-8`}>
               <div>
                 <h3 className="text-sm/6 font-semibold ">利用規約</h3>
-                <ul role="list" className="mt-6 space-y-4">
-                  {navigation.company.map((item) => (
+                <ul className="mt-6 space-y-4">
+                  {navigation.policy.map((item) => (
                     <li key={item.name}>
                       <Link href={item.href} className="text-sm/6  hover:text-blue-500">
                         {item.name}
@@ -117,10 +61,10 @@ export default function Footer() {
                   ))}
                 </ul>
               </div>
-              <div className="mt-10 md:mt-0 footerMenu">
+              <div className={`${styles.menuItem} mt-10 md:mt-0`}>
                 <h3 className="text-sm/6 font-semibold ">お問い合わせ</h3>
-                <ul role="list" className="mt-6 space-y-4">
-                  {navigation.legal.map((item) => (
+                <ul className="mt-6 space-y-4">
+                  {navigation.contact.map((item) => (
                     <li key={item.name}>
                       <Link href={item.href} className="text-sm/6  hover:text-blue-500">
                         {item.name}
@@ -133,7 +77,7 @@ export default function Footer() {
           </div>
         </div>
         <div className="mt-16 border-t border-gray-300 pt-8 sm:mt-20 lg:mt-24">
-          <p className="text-xs ">Copyright © 2024 リクビジョン All Rights Reserved.</p>
+          <p className="text-xs">Copyright © 2024 リクビジョン All Rights Reserved.</p>
         </div>
       </div>
     </footer>
