aca94ff4a8b5673fa070ffc0388fabadd4921f4b
Arata <aratadayoon@yahoo.co.jp>
2025-03-13 18:35:19 +0900

    refactor: footer

--- src/components/Common/Layouts/Footer/index.tsx
+++ src/components/Common/Layouts/Footer/index.tsx
@@ -1,20 +1,6 @@
 import Link from 'next/link';
 import styles from './index.module.css';
-
-const navigation = {
-  about: [
-    { name: 'アカウント登録', href: '/service/auth/signup' },
-    { name: 'ログイン', href: '/service/auth/login' },
-  ],
-  developer: [{ name: 'リアル大学生', href: 'https://realunivlog.com' }],
-  policy: [
-    { name: 'プライバシーポリシー', href: '/privacy' },
-    { name: '免責事項', href: '/disclaimer' },
-    { name: '著作権', href: '/copyright' },
-    { name: 'リンク', href: '/link' },
-  ],
-  contact: [{ name: 'お問い合わせ', href: '/contact' }],
-};
+import { FOOTER_NAVIGATION, PROJECT_IMAGE, COPYRIGHT } from '@/constants/data';
 
 export default function Footer() {
   return (
@@ -23,11 +9,14 @@ export default function Footer() {
         <div className="xl:grid xl:grid-cols-3 xl:gap-8">
           <div className="space-y-8">
             <Link href="/" className={styles.logo}>
-              <img
-                alt="タイトル"
-                src="/images/head/1.png"
-                className={`${styles.image} hover:scale-110 transition-transform`}
-              />
+              {PROJECT_IMAGE.map((item) => (
+                <img
+                  key={item.alt}
+                  alt={item.alt}
+                  src={item.path}
+                  className={`${styles.image} hover:scale-110 transition-transform`}
+                />
+              ))}
             </Link>
           </div>
           <div className="mt-16 grid grid-cols-2 gap-8 xl:col-span-2 xl:mt-0">
@@ -35,7 +24,7 @@ export default function Footer() {
               <div className={styles.menuItem}>
                 <h3 className="text-sm/6 font-semibold ">リクビジョン</h3>
                 <ul className="mt-6 space-y-4">
-                  {navigation.about.map((item) => (
+                  {FOOTER_NAVIGATION.about.map((item) => (
                     <li key={item.name}>
                       <Link href={item.href} className="text-sm/6  hover:text-blue-500">
                         {item.name}
@@ -52,7 +41,7 @@ export default function Footer() {
               <div>
                 <h3 className="text-sm/6 font-semibold ">利用規約</h3>
                 <ul className="mt-6 space-y-4">
-                  {navigation.policy.map((item) => (
+                  {FOOTER_NAVIGATION.policy.map((item) => (
                     <li key={item.name}>
                       <Link href={item.href} className="text-sm/6  hover:text-blue-500">
                         {item.name}
@@ -64,7 +53,7 @@ export default function Footer() {
               <div className={`${styles.menuItem} mt-10 md:mt-0`}>
                 <h3 className="text-sm/6 font-semibold ">お問い合わせ</h3>
                 <ul className="mt-6 space-y-4">
-                  {navigation.contact.map((item) => (
+                  {FOOTER_NAVIGATION.contact.map((item) => (
                     <li key={item.name}>
                       <Link href={item.href} className="text-sm/6  hover:text-blue-500">
                         {item.name}
@@ -77,7 +66,7 @@ export default function Footer() {
           </div>
         </div>
         <div className="mt-16 border-t border-gray-300 pt-8 sm:mt-20 lg:mt-24">
-          <p className="text-xs">Copyright © 2024 リクビジョン All Rights Reserved.</p>
+          <p className="text-xs">{COPYRIGHT}</p>
         </div>
       </div>
     </footer>
--- src/constants/data.ts
+++ src/constants/data.ts
@@ -1,2 +1,26 @@
 export const DESCRIPTION =
   '就活における日程管理や自己分析、選考状況などを一括で管理することのできるサービスです。';
+
+export const PROJECT_IMAGE = [
+  {
+    path: '/images/head/1.png',
+    alt: 'タイトル',
+  },
+];
+
+export const FOOTER_NAVIGATION = {
+  about: [
+    { name: 'アカウント登録', href: '/service/auth/signup' },
+    { name: 'ログイン', href: '/service/auth/login' },
+  ],
+  developer: [{ name: 'リアル大学生', href: 'https://realunivlog.com' }],
+  policy: [
+    { name: 'プライバシーポリシー', href: '/privacy' },
+    { name: '免責事項', href: '/disclaimer' },
+    { name: '著作権', href: '/copyright' },
+    { name: 'リンク', href: '/link' },
+  ],
+  contact: [{ name: 'お問い合わせ', href: '/contact' }],
+};
+
+export const COPYRIGHT = 'Copyright © 2024 リクビジョン All Rights Reserved.';
