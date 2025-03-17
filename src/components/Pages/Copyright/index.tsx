08634656a3a9f63d7c2b9ae032d633a508dea227
Arata <aratadayoon@yahoo.co.jp>
2025-03-17 15:58:05 +0900

    refactor: FixedPage

--- src/components/Pages/Copyright/index.tsx
+++ src/components/Pages/Copyright/index.tsx
@@ -1,5 +1,26 @@
-import CopyrightFeature from '@/components/Features/Copyright';
+import { DocumentTextIcon } from '@heroicons/react/24/solid';
+import { COPYRIGHT_CONTENT } from '@/contents/copyright';
+import AdUnit from '@/components/ThirdParties/GoogleAdSense/Elements/AdUnit';
+import HomeContainer from '@/components/Common/Layouts/Container/HomeContainer';
+import BreadCrumb from '@/components/Common/BreadCrumb';
+import HomeTitle from '@/components/Common/HomeTitle';
+import Date from '@/components/Common/Date';
+import Markdown from '@/components/Common/Markdown';
+import FixedMainContainer from '@/components/Common/Layouts/Container/FixedMainContainer';
+import FixedContentContainer from '@/components/Common/Layouts/Container/FixedContentContainer';
 
 export default function CopyrightPage() {
-  return <CopyrightFeature />;
+  return (
+    <HomeContainer white={true}>
+      <BreadCrumb title="著作権" path="copyright" />
+      <FixedMainContainer>
+        <FixedContentContainer>
+          <HomeTitle title="著作権" Icon={DocumentTextIcon} />
+          <Date date="2024年11月26日" />
+          <Markdown content={COPYRIGHT_CONTENT} />
+        </FixedContentContainer>
+        <AdUnit slot="7998948559" style={{ marginTop: '1.25rem' }} />
+      </FixedMainContainer>
+    </HomeContainer>
+  );
 }
--- src/components/Pages/Disclaimer/index.tsx
+++ src/components/Pages/Disclaimer/index.tsx
@@ -1,5 +1,26 @@
-import DisclaimerFeature from '@/components/Features/Disclaimer';
+import { ExclamationCircleIcon } from '@heroicons/react/24/solid';
+import { DISCLAIMER_CONTENT } from '@/contents/disclaimer';
+import AdUnit from '@/components/ThirdParties/GoogleAdSense/Elements/AdUnit';
+import HomeContainer from '@/components/Common/Layouts/Container/HomeContainer';
+import BreadCrumb from '@/components/Common/BreadCrumb';
+import HomeTitle from '@/components/Common/HomeTitle';
+import Date from '@/components/Common/Date';
+import Markdown from '@/components/Common/Markdown';
+import FixedMainContainer from '@/components/Common/Layouts/Container/FixedMainContainer';
+import FixedContentContainer from '@/components/Common/Layouts/Container/FixedContentContainer';
 
 export default function DisclaimerPage() {
-  return <DisclaimerFeature />;
+  return (
+    <HomeContainer white={true}>
+      <BreadCrumb title="免責事項" path="disclaimer" />
+      <FixedMainContainer>
+        <FixedContentContainer>
+          <HomeTitle title="免責事項" Icon={ExclamationCircleIcon} />
+          <Date date="2024年11月26日" />
+          <Markdown content={DISCLAIMER_CONTENT} />
+        </FixedContentContainer>
+        <AdUnit slot="7998948559" style={{ marginTop: '1.25rem' }} />
+      </FixedMainContainer>
+    </HomeContainer>
+  );
 }
